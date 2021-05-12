accd118f26058c4d8a10f0ebfa6effac04c7ff8f
Yan Waipann <yanwaipann@gmail.com>
2021-05-12 16:37:01 +0630

    Add ifttt webhook POST handler

---
+++ controllers/ifttt.js
@@ -0,0 +1,29 @@
+const Twitter = require('../helper/twitter-api');
+const fetch = require('node-fetch');
+
+// @desc    Handle webhook call from IFTTT, calls the twitter API to tweet.
+// @desc    IFTTT calls whenever the twitter user being watched tweets.
+// @route   POST /ifttt/user-tweeted
+// @access  Public
+exports.specificUserTweeted = async (req, res) => {
+  const text = req.body.split(' @splitter ')[0];
+  // username and link at indices 1 and 2 respectively
+
+  try {
+    const parameters = {
+      status: text,
+    };
+
+    const tweet = Twitter.tweets.statusesUpdate(parameters);
+
+    return res.status(200).json({
+      success: true,
+      tweet: tweet,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      error: true,
+      message: e.message,
+    });
+  }
+};
