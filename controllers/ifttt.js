b66fb1f473e662aa1ddc4af1bf7280c0dffcadfc
Yan Waipann <yanwaipann@gmail.com>
2021-05-13 16:52:07 +0630

    Integrate ifttt for the one, you know who

--- controllers/ifttt.js
+++ controllers/ifttt.js
@@ -1,4 +1,5 @@
 const Twitter = require('../helper/twitter-api');
+const fetch = require('node-fetch');
 
 const sendNotification = require('../helper/send-notification');
 
@@ -44,6 +45,44 @@ exports.specificUserTweeted = async (req, res) => {
     return res.status(500).json({
       error: true,
       message: message,
+    });
+  }
+};
+
+// @desc    Handle webhook call from IFTTT, then calls IFTTT back to tweet.
+// @desc    IFTTT calls whenever the twitter user being watched tweets.
+// @route   POST /ifttt/hmnoo/user-tweeted
+// @access  Public
+exports.handleIFTTTCallFromPerson = async (req, res) => {
+  const text = req.body.split(' @splitter ')[0];
+  // username and link at indices 1 and 2 respectively
+
+  try {
+    const IFTTT_URI = process.env.IFTTT_TRIGGER_EVENT_URI_HMNOO;
+    fetch(IFTTT_URI, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        value1: text,
+      }),
+    });
+
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (e) {
+    const message = 'Error calling IFTTT wehbook.';
+    const notiTitle = 'Error calling IFTTT from H';
+    const priority = 1;
+
+    await sendNotification(notiTitle, message, priority);
+
+    console.log(e);
+
+    return res.status(500).json({
+      error: true,
     });
   }
 };
--- routes/ifttt.js
+++ routes/ifttt.js
@@ -3,8 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 // Controllers
-const { specificUserTweeted } = require('../controllers/ifttt');
+const {
+  specificUserTweeted,
+  handleIFTTTCallFromPerson,
+} = require('../controllers/ifttt');
 
 router.route('/user-tweeted').post(specificUserTweeted);
+router.route('/hmnoo/user-tweeted').post(handleIFTTTCallFromPerson);
 
 module.exports = router;
