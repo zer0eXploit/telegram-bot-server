5e3f59c36f52280f548617aa2944ce5f2bc77a43
Yan Waipann <yanwaipann@gmail.com>
2021-05-14 19:17:36 +0630

    Add route to tweet on behalf of user

--- routes/twitter.js
+++ routes/twitter.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 // Controllers
-const { twitterLogin, twitterCallback } = require('../controllers/twitter');
+const {
+  twitterLogin,
+  twitterCallback,
+  tweetForYou,
+} = require('../controllers/twitter');
 
 router.route('/login/:username').get(twitterLogin);
 
-router.route('/callback').get(twitterCallback());
+router.route('/callback').get(twitterCallback);
+
+router.route('/tweet-for/:username').post(tweetForYou);
 
 module.exports = router;
