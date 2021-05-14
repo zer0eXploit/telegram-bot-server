457fd3c493ed4cb0b1d896be579df9a00ae5eb18
Yan Waipann <yanwaipann@gmail.com>
2021-05-14 18:21:37 +0630

    Import modules for twitter login

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 require('dotenv').config();
 
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
 
 const TelegramBot = require('./helper/telegram-bot');
+
 const app = express();
 
 // Static Folder Setup
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.text());
+app.use(passport.initialize());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {},
+  }),
+);
 
 // Routes
 const homeRoute = require('./routes/home');
 const iftttRoute = require('./routes/ifttt');
+const twitterRoute = require('./routes/twitter');
 
 app.use('/', homeRoute);
 app.use('/ifttt', iftttRoute);
+app.use('/twitter', twitterRoute);
 
 const PORT = process.env.PORT || '3000';
 
