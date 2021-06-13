9b60f20c19a346ee8fb309edcc46dba3b11b5aa6
Yan Waipann <yanwaipann@gmail.com>
2021-06-13 16:32:34 +0630

    Remove dev debug logs

--- helper/send-notification.js
+++ helper/send-notification.js
@@ -1,8 +1,6 @@
 const fetch = require('node-fetch');
 
 const sendNotification = (errorData) => {
-  console.log(errorData);
-  console.log('I am called!');
   const uri = process.env.TWEET_ERROR_WEBHOOK_URI;
   return fetch(uri, {
     method: 'POST',
