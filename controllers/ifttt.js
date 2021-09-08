a1a8e835e5b8356c0664db17e440292703c793a3
Yan Waipann <yanwaipann@gmail.com>
2021-09-08 19:28:17 +0630

    Remove method handleIFTTTCallFromPerson

--- controllers/ifttt.js
+++ controllers/ifttt.js
@@ -1,7 +1,6 @@
-const Twitter = require('../helper/twitter-api');
-const fetch = require('node-fetch');
+const Twitter = require("../helper/twitter-api");
 
-const sendNotification = require('../helper/send-notification');
+const sendNotification = require("../helper/send-notification");
 
 // @desc    Handle webhook call from IFTTT, calls the twitter API to tweet.
 // @desc    IFTTT calls whenever the twitter user being watched tweets.
@@ -18,7 +17,7 @@ exports.specificUserTweeted = async (req, res) => {
   // }
 
   try {
-    const text = req.body.split(' @splitter ')[0];
+    const text = req.body.split(" @splitter ")[0];
     // username and link at indices 1 and 2 respectively
 
     const parameters = { status: text, trim_user: true };
@@ -57,42 +56,6 @@ exports.specificUserTweeted = async (req, res) => {
     return res.status(500).json({
       error: true,
       message: message,
-    });
-  }
-};
-
-// @desc    Handle webhook call from IFTTT, then calls IFTTT back to tweet.
-// @desc    IFTTT calls whenever the twitter user being watched tweets.
-// @route   POST /ifttt/hmnoo/user-tweeted
-// @access  Public
-exports.handleIFTTTCallFromPerson = async (req, res) => {
-  const text = req.body.split(' @splitter ')[0];
-  // username and link at indices 1 and 2 respectively
-
-  try {
-    const IFTTT_URI = process.env.IFTTT_TRIGGER_EVENT_URI_HMNOO;
-    fetch(IFTTT_URI, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        value1: text,
-      }),
-    });
-
-    return res.status(200).json({
-      success: true,
-    });
-  } catch (e) {
-    const message = 'Error calling IFTTT wehbook.';
-
-    await sendNotification({ value1: message });
-
-    console.log(e);
-
-    return res.status(500).json({
-      error: true,
     });
   }
 };
