a87b229128b2824c6a24839ebe04c6d7f56a1989
Yan Waipann <yanwaipann@gmail.com>
2021-09-08 22:37:49 +0630

    Change function parameter name to be more meaningful

--- helper/send-notification.js
+++ helper/send-notification.js
@@ -1,13 +1,13 @@
 const fetch = require("node-fetch");
 
-const sendNotification = (message) => {
+const sendNotification = (notiInfo) => {
   const uri = process.env.IFTTT_WEBHOOK_URL;
   return fetch(uri, {
     method: "POST",
     headers: {
       "content-type": "application/json",
     },
-    body: JSON.stringify(message),
+    body: JSON.stringify(notiInfo),
   });
 };
 
