d8fe7c32b081a672fed7f47a39b720607c98834c
Yan Waipann <yanwaipann@gmail.com>
2021-09-08 23:27:39 +0630

    Update function description, change return value

--- helper/air-table.js
+++ helper/air-table.js
@@ -4,16 +4,16 @@ const apiKey = process.env.AIR_TABLE_API_KEY;
 const base = new Airtable({ apiKey }).base("appRCF5ZGpwgJVzd6");
 
 /**
- * @desc    Checks if a user exists inside the air table view
- * @param   {string} username - The username required to check
- * @return  {boolean} If the user exists or not
+ * @desc    Search for a user by username inside airtable view.
+ * @param   {String} username - The username required to filter.
+ * @return  {Array} Record found that is found or empty array if not.
  */
 const userExists = async (username) => {
   const filterByFormula = `{username} = '${username}'`;
   const records = await base("AllowedUsers")
     .select({ filterByFormula })
     .firstPage();
-  return !!records.length;
+  return records;
 };
 
 module.exports = userExists;
