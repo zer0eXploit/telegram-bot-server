e921a3703d741b24f9636d29a2eb488b067446c5
Yan Waipann <yanwaipann@gmail.com>
2021-09-11 16:24:35 +0630

    Add regexes of commands to constants

---
+++ helper/commands/constants/regexps.js
@@ -0,0 +1,12 @@
+const botUsername = process.env.BOT_USERNAME;
+
+const commands = {
+  INTRODUCE: new RegExp(`^(\/introduce|\/introduce@${botUsername})`),
+  GREETINGS: new RegExp(`/^(Hello|Hi|Greetings|Greeting|Sup|Hey)/i`),
+  ECHO: new RegExp(
+    `(^(\/echo|\/echo@${botUsername}) (.+))|^(\/echo|\/echo@${botUsername})$`,
+  ),
+  TELL_A_JOKE: new RegExp(`^(\/tell_a_joke|\/tell_a_joke@${botUsername})$`),
+};
+
+module.exports = commands;
