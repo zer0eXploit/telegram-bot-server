01263032758518ee30299d247f462dd9aa87160f
Yan Waipann <yanwaipann@gmail.com>
2021-09-11 17:06:04 +0630

    Add redis db connector

---
+++ config/redis-db.js
@@ -0,0 +1,20 @@
+const redis = require("redis");
+
+const connectRedisDB = () => {
+  const config = {
+    url: process.env.REDIS_URI,
+  };
+  const client = redis.createClient(config);
+
+  client.on("connect", function () {
+    console.log("Redis connection established!");
+  });
+
+  client.on("error", function (error) {
+    console.error(error);
+  });
+
+  return client;
+};
+
+module.exports = connectRedisDB;
